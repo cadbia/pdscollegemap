ea89ac809d33058ae805208cf54987452efd4558
cadbia <cadenbialow@icloud.com>
2023-01-05 01:55:03 -0500

    cors

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ from bokeh.models import HoverTool
 from geoviews import dim, opts
 import numpy as np
 from bokeh.embed import components
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
